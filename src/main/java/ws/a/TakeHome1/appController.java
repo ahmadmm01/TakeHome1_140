96cac2543d7378cb59a254d01c579f797c5437a4
ahmadmm01 <94947950+ahmadmm01@users.noreply.github.com>
2022-10-31 11:30:31 +0700

    add parameter for method in controller

--- src/main/java/ws/a/TakeHome1/appController.java
+++ src/main/java/ws/a/TakeHome1/appController.java
@@ -4,10 +4,28 @@
  */
 package ws.a.TakeHome1;
 
+import java.io.IOException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  *
  * @author MADD
  */
-public class appController {
-    
+@Controller
+public class appController
+{
+    @RequestMapping("/nexthalaman")
+    public String next(
+                @RequestParam(value = "varA") String data1,
+                @RequestParam(value = "varB") String data2,
+                @RequestParam(value = "varC") MultipartFile data3,
+                Model Courier
+                )throws IOException
+    {
+        return "viewpage";
+    }
 }
